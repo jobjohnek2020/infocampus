66554506378d8182f1ad4881d8551225883348a2
Jobin John <jobjohnek2020@gmail.com>
2025-01-13 19:52:07 +0530

    added changes related to meal finder

--- meal-finder/src/components/MealRecipeBasic.js
+++ meal-finder/src/components/MealRecipeBasic.js
@@ -3,8 +3,8 @@ export default function MealRecipeBasic({ meal }) {
 
     return (
         <div className="px-2">
-            <h2 className="mt-2">{meal.strMeal}</h2>
-            <h5>Ingredients</h5>
+            <h1 className="mt-2 display-1">{meal.strMeal}</h1>
+            <h6 className="lead display-7">Ingredients</h6>
             <ul className="list-unstyled ingredient-scroll">
                 <MealIngredients meal={meal} />
             </ul>
